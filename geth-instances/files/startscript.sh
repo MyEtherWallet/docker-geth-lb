c4e8dab4234282455d38339e0956efd2b363df27
Kosala Hemachandra <kvhnuke@aol.com>
2018-06-22 19:27:35 -0700

    geth instance updated with --etstats flag

--- geth-instances/files/startscript.sh
+++ geth-instances/files/startscript.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-cd /root/eth-net-intelligence-api/
-/usr/bin/pm2 start app.json
 while true
 do
 if [ ! `pgrep geth` ] ; then
-	geth --cache=512 --rpcvhosts='*' --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*' --txpool.accountslots=256 --txpool.globalslots=20000 --txpool.accountqueue=256 --txpool.globalqueue=20000
+	geth --ethstats "$INSTANCE_NAME:$WS_SECRET@$WS_SERVER:3000" --cache=512 --rpcvhosts='*' --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*' --txpool.accountslots=256 --txpool.globalslots=20000 --txpool.accountqueue=256 --txpool.globalqueue=20000
 fi
 sleep 10
 done
