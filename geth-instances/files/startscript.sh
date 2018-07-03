e4fed96dfd419006f2610ba7d3fc37fa04109516
Kosala Hemachandra <kvhnuke@aol.com>
2018-07-03 16:06:12 -0700

    removed tx pool settngs

--- geth-instances/files/startscript.sh
+++ geth-instances/files/startscript.sh
@@ -2,7 +2,7 @@
 while true
 do
 if [ ! `pgrep geth` ] ; then
-	geth --ethstats "$INSTANCE_NAME:$WS_SECRET@$WS_SERVER:3000" --cache=512 --rpcvhosts='*' --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*' --txpool.accountslots=256 --txpool.globalslots=20000 --txpool.accountqueue=256 --txpool.globalqueue=20000
+	geth --ethstats "$INSTANCE_NAME:$WS_SECRET@$WS_SERVER:3000" --cache=512 --rpcvhosts='*' --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*'
 fi
 sleep 10
 done
