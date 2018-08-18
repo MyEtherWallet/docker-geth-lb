63d3f17eef5ccf811645e55bd84f4e24e67efd03
Kosala Hemachandra <kvhnuke@aol.com>
2018-08-17 18:20:11 -0700

    tx persistance is important

--- parity-prebuilt-instance/files/startscript.sh
+++ parity-prebuilt-instance/files/startscript.sh
@@ -4,7 +4,7 @@ cd /root/eth-net-intelligence-api/
 while true
 do
 if [ ! `pgrep parity` ] ; then
-	parity --rpccorsdomain '*' --rpcaddr '0.0.0.0' --jsonrpc-hosts 'all' --tx-queue-no-unfamiliar-locals --no-persistent-txqueue
+	parity --rpccorsdomain '*' --rpcaddr '0.0.0.0' --jsonrpc-hosts 'all' --tx-queue-no-unfamiliar-locals
 fi
 sleep 10
 done
