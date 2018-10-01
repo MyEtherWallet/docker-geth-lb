6bf098671b1baeb7684b7874eb295c0d796cbb42
Kosala Hemachandra <kvhnuke@aol.com>
2018-10-01 13:26:23 -0700

    set coinbase address to stop netstats from failing

--- parity-prebuilt-instance/files/startscript.sh
+++ parity-prebuilt-instance/files/startscript.sh
@@ -4,7 +4,7 @@ cd /root/eth-net-intelligence-api/
 while true
 do
 if [ ! `pgrep parity` ] ; then
-	parity --rpccorsdomain '*' --rpcaddr '0.0.0.0' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --tx-queue-no-unfamiliar-locals --no-persistent-txqueue --tx-queue-size 16384
+	parity --author 0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D --rpccorsdomain '*' --rpcaddr '0.0.0.0' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --tx-queue-no-unfamiliar-locals --no-persistent-txqueue --tx-queue-size 16384
 fi
 sleep 10
 done
