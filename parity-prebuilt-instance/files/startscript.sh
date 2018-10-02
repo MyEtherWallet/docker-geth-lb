c648f57ca371fe0d1d5a87971fad608fd0c45643
Kosala Hemachandra <kvhnuke@aol.com>
2018-10-02 15:25:51 -0700

    add json rpc server threads

--- parity-prebuilt-instance/files/startscript.sh
+++ parity-prebuilt-instance/files/startscript.sh
@@ -4,7 +4,7 @@ cd /root/eth-net-intelligence-api/
 while true
 do
 if [ ! `pgrep parity` ] ; then
-	parity --jsonrpc-threads 32 --author 0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D --jsonrpc-cors 'all' --jsonrpc-interface 'all' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --tx-queue-no-unfamiliar-locals --no-persistent-txqueue --tx-queue-mem-limit 1024 --tx-queue-size 128000 --tx-time-limit 100
+	parity --jsonrpc-server-threads 8 --jsonrpc-threads 32 --author 0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D --jsonrpc-cors 'all' --jsonrpc-interface 'all' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --tx-queue-no-unfamiliar-locals --no-persistent-txqueue --tx-queue-mem-limit 1024 --tx-queue-size 128000 --tx-time-limit 100
 fi
 sleep 10
 done
