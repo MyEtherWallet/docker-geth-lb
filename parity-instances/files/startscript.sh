c4558c55ea1dd8364a9ec213f869f94e972c1374
Kosala Hemachandra <kvhnuke@aol.com>
2018-10-02 17:33:28 -0700

    remove multi threading

--- parity-instances/files/startscript.sh
+++ parity-instances/files/startscript.sh
@@ -4,7 +4,7 @@ cd /root/eth-net-intelligence-api/
 while true
 do
 if [ ! `pgrep parity` ] ; then
-	parity --jsonrpc-server-threads 8 --jsonrpc-threads 64 --author 0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D --jsonrpc-cors 'all' --jsonrpc-interface 'all' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --no-persistent-txqueue --tx-queue-mem-limit 1024 --tx-queue-size 16384 --tx-time-limit 100
+	parity --author 0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D --jsonrpc-cors 'all' --jsonrpc-interface 'all' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --no-persistent-txqueue --tx-queue-mem-limit 1024 --tx-queue-size 16384 --tx-time-limit 100
 fi
 sleep 10
 done
