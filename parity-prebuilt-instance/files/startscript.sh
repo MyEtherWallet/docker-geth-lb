c6fad5edf067cd0c393822b8c5848779958acf1f
Kosala Hemachandra <kvhnuke@aol.com>
2019-01-17 22:48:33 -0800

    increase max ws connections

--- parity-prebuilt-instance/files/startscript.sh
+++ parity-prebuilt-instance/files/startscript.sh
@@ -7,7 +7,7 @@ while true
 do
     if [ ! `pgrep parity` ] ; then
         parity --author 0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D --jsonrpc-cors 'all' --jsonrpc-interface 'all' --jsonrpc-hosts 'all' --min-gas-price 1000000000 --no-persistent-txqueue --tx-queue-mem-limit 1024 --tx-queue-size 16384 --tx-time-limit 100 \
-        --ws-interface 'all' --ws-origins $ALLOWED_ORIGINS --ws-hosts $ALLOWED_HOSTS
+        --ws-interface 'all' --ws-origins $ALLOWED_ORIGINS --ws-hosts $ALLOWED_HOSTS --ws-max-connections 1000
     fi
     sleep 10
 done
